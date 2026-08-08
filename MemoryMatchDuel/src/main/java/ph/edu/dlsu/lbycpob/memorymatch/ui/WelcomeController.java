2aeac4aeccaade58b7e672b4a98776ca96473a38
Matthew Tiu <matthew_gabriel_tiu@dlsu.edu.ph>
2026-08-09 05:04:53 +0800

    fix: Welcome Controller handling

---
+++ MemoryMatchDuel/src/main/java/ph/edu/dlsu/lbycpob/memorymatch/ui/WelcomeController.java
@@ -0,0 +1,33 @@
+package ph.edu.dlsu.lbycpob.memorymatch.ui;
+
+import javafx.fxml.FXML;
+
+public class WelcomeController {
+
+    @FXML
+    private void handlePlay() {
+
+        SceneManager.get().switchTo(
+                "/fxml/name_entry.fxml",
+                "Memory Match Showdown - Players"
+        );
+    }
+
+    @FXML
+    private void handleLeaderboard() {
+
+        SceneManager.get().switchTo(
+                "/fxml/leaderboard.fxml",
+                "Memory Match Showdown - Leaderboard"
+        );
+    }
+
+    @FXML
+    private void handleTournament() {
+
+        SceneManager.get().switchTo(
+                "/fxml/tournament.fxml",
+                "Memory Match Showdown - Tournament"
+        );
+    }
+}
